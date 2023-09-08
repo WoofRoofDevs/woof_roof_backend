6908e1db9ca4def786dc42fde1664052beb14f34
Sasha Kononenko <nurchiksasha@gmail.com>
2023-09-09 01:13:15 +0300

    added CORS

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import cors from "cors";
 import mongoose from "mongoose";
 import { RequestLoggerModel } from "./models/RequestLogger";
 import { Request, Response } from "express";
 
 const app = express();
+
+app.use(cors());
 
 app.set("trust proxy", true);
 
